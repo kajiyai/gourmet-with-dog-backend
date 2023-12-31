ce693d68904944ebb01f0f6d5041a28201d2a274
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2023-12-31 18:08:41 +0900

    犬種リソースのコントローラを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :dogs
+  resources :dog_breeds, only: [:index]
   namespace :v1, { format: 'json' } do
     mount_devise_token_auth_for 'User', at: 'auth', controllers: {
       registrations: "v1/auth/registrations"
