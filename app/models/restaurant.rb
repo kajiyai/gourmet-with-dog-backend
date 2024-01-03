1a5f848ef3904b2f17ca60a3357d6b6804a60294
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2024-01-03 17:10:43 +0900

    レストランテーブルのカラムのデータ型の変更

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,29 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id                 :uuid             not null, primary key
+#  feature            :text
+#  image_url          :string
+#  max_price          :decimal(, )
+#  min_price          :decimal(, )
+#  name               :string
+#  pet_friendly_score :integer
+#  phone_number       :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  address_id         :uuid
+#  amenity_id         :uuid
+#  category_id        :uuid
+#  owner_id           :uuid
+#
+# Indexes
+#
+#  index_restaurants_on_address_id   (address_id)
+#  index_restaurants_on_amenity_id   (amenity_id)
+#  index_restaurants_on_category_id  (category_id)
+#  index_restaurants_on_owner_id     (owner_id)
+#
 class Restaurant < ApplicationRecord
   belongs_to :owner
   belongs_to :category, foreign_key: 'category_id'
