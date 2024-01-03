9a265a260b80b95d232a9fbbbd37dd41f76692f7
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2024-01-03 16:59:22 +0900

    関連するテーブル名の変更

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
   belongs_to :owner
-  belongs_to :categorymaster, foreign_key: 'category_id'
-  belongs_to :amenitymaster, foreign_key: 'amenity_id'
+  belongs_to :category, foreign_key: 'category_id'
+  belongs_to :amenity, foreign_key: 'amenity_id'
 
   validates :name, presence: true
   validates :phone_number, presence: true
