0160cd5fb23db44b8837293d1ca7da06924b4920
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2024-01-04 20:11:49 +0900

    中間テーブルの作成

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :uuid             not null, primary key
+#  icon_url   :string
+#  name       :string           not null
+#  type       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  parent_id  :uuid
+#
+# Indexes
+#
+#  index_categories_on_parent_id  (parent_id)
+#
 class Category < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_04_104400) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_04_111043) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -38,6 +38,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_04_104400) do
     t.datetime "updated_at", null: false
     t.uuid "parent_id"
     t.index ["parent_id"], name: "index_categories_on_parent_id"
+  end
+
+  create_table "categories_restaurants", id: false, force: :cascade do |t|
+    t.bigint "restaurant_id", null: false
+    t.bigint "category_id", null: false
   end
 
   create_table "dog_breeds", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
