5fd97870e6e1cd8f1e8f0526d566f202a9ba9872
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2024-01-08 23:03:39 +0900

    20個レストランを作成するようにシードを変更

--- db/seeds/restaurants.rb
+++ db/seeds/restaurants.rb
@@ -1,35 +1,29 @@
 puts 'レストランを作成しています...'
 
 begin
-  # 特定のオーナーを名前で検索
-  owner = Owner.find_by(name: 'オーナー1')
+  20.times do |i|
+    # 特定のオーナーを名前で検索
+    owner = Owner.find_by(name: 'オーナー1')
+    raise '指定されたオーナーが見つかりません。' if owner.nil?
 
-  # オーナーが見つからない場合のエラーハンドリング
-  if owner.nil?
-    raise '指定されたオーナーが見つかりません。'
-  end
+    # 住所情報を検索
+    address = Address.offset(rand(Address.count)).first
 
-  # 住所情報を検索または作成
-  address = Address.find_by!(
-    street: '中央通り1-2-3',
-    city: '東京市',
-    prefecture: '東京都',
-    postal_code: '100-0001'
-  )
+    # レストランデータを作成
+    restaurant = Restaurant.find_or_create_by!(name: "レストラン#{i+1}") do |r|
+      r.phone_number = "123-456-78#{i+1}"
+      r.owner_id = owner.id
+      r.address_id = address.id
+    end
 
-  # サンプルデータとしてレストランを作成
-  restaurant = Restaurant.find_or_create_by!(name: 'レストラン名') do |r|
-    r.phone_number = '123-456-7890'
-    r.owner_id = owner.id
-    r.address_id = address.id
-  end
-
-  if restaurant.persisted?
-    restaurant.categories << Category.where(name: ['イタリアン', '和食']).to_a
-    restaurant.amenities << Amenity.where(name: ['Wi-Fi', 'ペットフレンドリー']).to_a
-    puts "レストラン '#{restaurant.name}' の作成に成功しました。"
-  else
-    puts "レストラン '#{restaurant.name}' の作成に失敗しました。"
+    if restaurant.persisted?
+      # カテゴリーとアメニティをランダムに選択して紐づける
+      restaurant.categories << Category.where(name: ['イタリアン', '和食', '寿司', 'フレンチ', 'ビストロ']).sample(2)
+      restaurant.amenities << Amenity.where(name: ['Wi-Fi', '駐車場', '喫煙エリア', 'ペットフレンドリー', 'ドッグラン']).sample(2)
+      puts "レストラン '#{restaurant.name}' の作成に成功しました。"
+    else
+      puts "レストラン '#{restaurant.name}' の作成に失敗しました。"
+    end
   end
 rescue => e
   puts "レストランの作成中にエラーが発生しました: #{e.message}"
