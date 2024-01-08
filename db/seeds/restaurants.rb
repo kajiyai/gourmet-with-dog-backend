abbcad89c4ac6ed428e867ec7543ea9143a710b0
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2024-01-08 13:33:24 +0900

    レストランシードの改善

--- db/seeds/restaurants.rb
+++ db/seeds/restaurants.rb
@@ -5,11 +5,11 @@ begin
   restaurant = Restaurant.find_or_create_by!(name: 'レストラン名') do |r|
     r.phone_number = '123-456-7890'
     r.owner_id = Owner.first.id
-    r.categories << Category.where(name: ['イタリアン', '和食']).to_a
-    r.amenities << Amenity.where(name: ['Wi-Fi', 'ペットフレンドリー']).to_a
   end
 
   if restaurant.persisted?
+    restaurant.categories << Category.where(name: ['イタリアン', '和食']).to_a
+    restaurant.amenities << Amenity.where(name: ['Wi-Fi', 'ペットフレンドリー']).to_a
     puts "レストラン '#{restaurant.name}' の作成に成功しました。"
   else
     puts "レストラン '#{restaurant.name}' の作成に失敗しました。"
