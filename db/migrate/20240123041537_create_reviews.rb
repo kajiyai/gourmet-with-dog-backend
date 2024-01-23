540f270732e36b2aefa59e9d28b4caef9ebad807
kajiyai <kajihara.ryoga.qd@alumni.tsukuba.ac.jp>
2024-01-23 13:18:38 +0900

    レビューモデルの作成

---
+++ db/migrate/20240123041537_create_reviews.rb
@@ -0,0 +1,17 @@
+class CreateReviews < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reviews, id: :uuid do |t|
+      t.uuid :user_id
+      t.uuid :restaurant_id
+      t.integer :evaluation
+      t.text :comment
+      t.datetime :visited_at
+      t.string :image_url
+
+      t.timestamps
+    end
+
+    add_index :reviews, :user_id
+    add_index :reviews, :restaurant_id
+  end
+end
